a5c039b2b256e25a4df32c242510e70e1ff59be0
Michael McLarnon <mmclar@gmail.com>
2023-04-20 00:09:01 -0400

    skip scraping recently updated jobs

--- backend/scrape/scraper.py
+++ backend/scrape/scraper.py
@@ -1,17 +1,19 @@
 import asyncio
+import datetime
 import logging
-import urllib.request
+from itertools import groupby
 
 from django.conf import settings
 from django.utils import timezone
 from playwright.async_api import async_playwright
 
-from jvapp.models import Employer
+from jvapp.models import Employer, EmployerJob
 from scrape.employer_scrapers import all_scrapers
 from scrape.job_processor import JobProcessor
 
 logger = logging.getLogger(__name__)
 JS_LOAD_WAIT_MS = 30000 if settings.DEBUG else 60000
+SKIP_SCRAPE_CUTOFF = datetime.timedelta(days=7)
 
 
 async def get_browser(playwright):
@@ -21,11 +23,11 @@ async def get_browser(playwright):
     return browser_context
 
         
-async def launch_scrapers(scraper_classes):
+async def launch_scrapers(scraper_classes, skip_urls):
     # Scrape jobs from web pages
     async with async_playwright() as p:
         browser = await get_browser(p)
-        scrapers = [sc(p, browser) for sc in scraper_classes]
+        scrapers = [sc(p, browser, skip_urls) for sc in scraper_classes]
         try:
             async_scrapers = [s.scrape_jobs() for s in scrapers]
             await asyncio.gather(*async_scrapers)
@@ -42,7 +44,18 @@ def run_job_scrapers(employer_names=None):
         scraper_classes = all_scrapers.values()
     else:
         scraper_classes = [all_scrapers[employer_name] for employer_name in employer_names]
-    
+
+    # Make a map of employer -> list of recently scraped urls
+    recent_employer_jobs = (
+        EmployerJob.objects
+        .filter(modified_dt__gt=datetime.datetime.now() - SKIP_SCRAPE_CUTOFF)
+        .order_by('employer_id')
+    )
+    skip_urls_by_employer_name = {
+        k: [ej.application_url for ej in v]
+        for k, v in groupby(recent_employer_jobs, key=lambda ej: ej.employer.employer_name)
+    }
+
     for scraper_class in scraper_classes:
         # Allow scrapers to fail so it doesn't impact other scrapers
         employer = None
@@ -60,7 +73,7 @@ def run_job_scrapers(employer_names=None):
                     logger.info(f'Scraper successfully run in last 3 hours for {scraper_class.employer_name}. Skipping')
                     continue
             
-            scrapers = asyncio.run(launch_scrapers([scraper_class]))
+            scrapers = asyncio.run(launch_scrapers([scraper_class], skip_urls_by_employer_name))
         
             # Process raw job data
             job_processor = JobProcessor()
