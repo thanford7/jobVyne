1849a39ca07569f5ddd13f464e70a99b6ef54005
Michael McLarnon <mmclar@gmail.com>
2023-05-04 09:31:02 -0400

    - set employee bits - ensure emails are permitted in employer domains

--- backend/jvapp/tests/test_csv.py
+++ backend/jvapp/tests/test_csv.py
@@ -5,13 +5,13 @@ from jvapp.tests.base import BaseTestCase
 from jvapp.utils import csv
 
 
-class JobApplicationTestCase(BaseTestCase):
-    def test_simple(self):
-        employer = models.Employer.objects.filter().first()
+class CsvUploadTest(BaseTestCase):
+    def test_csv(self):
+        employer = models.Employer.objects.filter(employer_name='ChickFilet').first()
         self.assertEqual(0, models.JobVyneUser.objects.filter(email='michael@jobvyne.com').count())
         csv_text = '\n'.join([
-            "email,first_name,last_name",
-            "mark@jobvyne.com,Marky,Mark",
+            "email,first_name,last_name,user_type_bits",
+            "mark@jobvyne.com,Marky,  Mark,1",
         ])
         with StringIO(csv_text) as csv_file:
             csv.bulk_load_users(csv_file, employer)
@@ -19,6 +19,7 @@ class JobApplicationTestCase(BaseTestCase):
         initial_id = user.id
         self.assertEqual('Marky', user.first_name)
         self.assertEqual('Mark', user.last_name)
+        self.assertEqual(models.JobVyneUser.USER_TYPE_EMPLOYEE, user.user_type_bits)
         self.assertEqual(employer, user.employer)
         initial_mod_time = user.modified_dt
 
@@ -35,3 +36,28 @@ class JobApplicationTestCase(BaseTestCase):
         self.assertEqual(initial_id, user.id)
         # Make sure the mod time has been updated via save signals
         self.assertGreater(user.modified_dt, initial_mod_time)
+
+        # Make sure the email domain rules are followed when an employer has email domains set
+        employer.email_domains = 'chickfilet.com'
+        employer.save()
+        csv_text = '\n'.join([
+            "email,first,last",
+            "employee@chickfilet.com,Workhere,Truly",
+            "nonemployee@chickfilb.com,Donut,Workhere",
+        ])
+        with StringIO(csv_text) as csv_file:
+            csv.bulk_load_users(csv_file, employer)
+        # NEITHER of the two employees shall have been added
+        users = models.JobVyneUser.objects.filter(email__in=['employee@chickfilet.com', 'nonemployee@chickfilb.com'])
+        self.assertFalse(users.exists())
+
+        csv_text = '\n'.join([
+            "email,first,last",
+            "employee@chickfilet.com,Workhere,Truly",
+        ])
+        with StringIO(csv_text) as csv_file:
+            csv.bulk_load_users(csv_file, employer)
+        # The employee shall have been added
+        users = models.JobVyneUser.objects.filter(email='employee@chickfilet.com')
+        self.assertEqual(1, users.count())
+
--- backend/jvapp/utils/csv.py
+++ backend/jvapp/utils/csv.py
@@ -1,9 +1,15 @@
 import csv
+import logging
 from dataclasses import dataclass
+from functools import partial
 from typing import Iterable
 
-from jvapp import models
+from django.db.transaction import atomic
 
+from jvapp import models
+from jvapp.utils.email import get_domain_from_email
+
+logger = logging.getLogger(__name__)
 
 @dataclass
 class FieldDefinition:
@@ -12,19 +18,29 @@ class FieldDefinition:
     aliases: Iterable[str] = ()
     default: object = None
     override: object = None
+    validate_fn: callable = None
 
-    def get_value(self, row, colIndex, original_val):
-        if 0 <= colIndex < len(row):
-            return row[colIndex] or self.default
-        return self.override or original_val or self.default
+    class InvalidFieldData(Exception):
+        pass
 
+    def get_value(self, row, colIndex, original_val=None):
+        val = row[colIndex].strip() if 0 <= colIndex < len(row) else original_val
+        self.validate_fn and self.validate_fn(val)
+        return self.override or val or self.default
 
-def normalize(s):
+def normalize_field_name(s):
     return s.replace('_', '').lower()
 
+def identity(o):
+    return o
+
+def create_from_model(model, *args, **kwargs):
+    obj = model(*args, **kwargs)
+    obj.save()
+    return obj
 
 def is_column_match(normalized_header, field_definition):
-    normalized_model_field = normalize(field_definition.model_field)
+    normalized_model_field = normalize_field_name(field_definition.model_field)
     if normalized_model_field == normalized_header:
         return True
     for alias in field_definition.aliases:
@@ -34,15 +50,27 @@ def is_column_match(normalized_header, field_definition):
 
 
 def bulk_load_users(csv_file, employer):
-    return bulk_load_objects(csv_file, models.JobVyneUser, [
-        FieldDefinition(model_field='email', aliases=['emailaddress'], is_key=True),
-        FieldDefinition(model_field='first_name', aliases=['first']),
-        FieldDefinition(model_field='last_name', aliases=['last']),
-        FieldDefinition(model_field='employer', default=employer),
-    ])
+    def email_check(email):
+        if employer.email_domains:
+            if not get_domain_from_email(email) in employer.email_domains:
+                raise FieldDefinition.InvalidFieldData(f'Email {email} not allowed for employer {employer}')
+
+    try:
+        with atomic():
+            return bulk_load_objects(csv_file, models.JobVyneUser, [
+                FieldDefinition(model_field='email', aliases=['emailaddress'], is_key=True, validate_fn=email_check),
+                FieldDefinition(model_field='first_name', aliases=['first']),
+                FieldDefinition(model_field='last_name', aliases=['last']),
+                FieldDefinition(model_field='employer', default=employer),
+                FieldDefinition(model_field='user_type_bits', override=models.JobVyneUser.USER_TYPE_EMPLOYEE),
+            ], instance_create_fn=models.JobVyneUser.objects.create_user)
+    except FieldDefinition.InvalidFieldData:
+        logger.info('Error uploading CSV')
 
 
-def bulk_load_objects(csv_file, model, field_definitions):
+def bulk_load_objects(csv_file, model, field_definitions, instance_create_fn=None, key_clean_fn=None):
+    instance_create_fn = instance_create_fn or partial(create_from_model, model)
+    key_clean_fn = key_clean_fn or identity
     dialect = csv.Sniffer().sniff(csv_file.read())
     csv_file.seek(0)
 
@@ -51,7 +79,7 @@ def bulk_load_objects(csv_file, model, field_definitions):
 
     field_map = []  # Mapping of row_index -> field_definition
     for idx, header in enumerate(header_row):
-        normalized_header = normalize(header)
+        normalized_header = normalize_field_name(header)
         has_been_matched = False
         for field_definition in field_definitions:
             if has_been_matched:
@@ -63,30 +91,30 @@ def bulk_load_objects(csv_file, model, field_definitions):
                 has_been_matched = True
     unmapped_fields = [fd for fd in field_definitions if fd not in field_map]
 
-    new_records = [
-        model(**{field_def.model_field: field_def.get_value(row, i, None) or field_definition.default for i, field_def in enumerate(field_map + unmapped_fields)})
-        for row in reader
-    ]
+    data_rows = [row for row in reader]
 
-    # If there's a key field, identify those objects and update (rather than create) them
-    key_field = next((fd.model_field for fd in field_definitions if fd.is_key), None)
+    # If there's a key field, identify update rows and update them
+    key_idx, key_field = next(((key_idx, fd.model_field) for key_idx, fd in enumerate(field_definitions) if fd.is_key), None)
     if key_field:
-        objects_by_key = {getattr(o, key_field): o for o in new_records}
-        existing_records = model.objects.filter(**{f'{key_field}__in': objects_by_key.keys()})
-        for existing_records in existing_records:
-            key = getattr(existing_records, key_field)
-            new = objects_by_key[key]
-            for field_definition in field_definitions:
+        data_rows_by_key = {key_clean_fn(row[key_idx]): row for row in data_rows}
+        existing_records = model.objects.filter(**{f'{key_field}__in': data_rows_by_key.keys()})
+        for existing_record in existing_records:
+            key = getattr(existing_record, key_field)
+            update_row = data_rows_by_key[key]
+            for idx, field_definition in enumerate(field_definitions):
                 if field_definition.model_field != key_field:
-                    val = field_definition.get_value([], -1, getattr(new, field_definition.model_field))
-                    setattr(existing_records, field_definition.model_field, val)
-            objects_by_key.pop(key)
-            existing_records.save()
-        # model.objects.bulk_update(existing_records, (fd.model_field for fd in field_definitions if not fd.is_key))
-        new_records = list(objects_by_key.values())
+                    val = field_definition.get_value(update_row, idx, getattr(existing_record, field_definition.model_field))
+                    setattr(existing_record, field_definition.model_field, val)
+            data_rows_by_key.pop(key)
+            existing_record.save()
+        data_rows = list(data_rows_by_key.values())
 
-    # model.objects.bulk_create(new_records)
-    for new_record in new_records:
-        new_record.save()
+    for row in data_rows:
+        kwargs = {
+            fd.model_field: fd.get_value(row, idx)
+            for idx, fd in enumerate(field_map + unmapped_fields)
+        }
+        instance_create_fn(**kwargs)
+
 
 
